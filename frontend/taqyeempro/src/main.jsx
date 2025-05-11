117c2f36440778f725867a258725937f58c8f50b
Hawraalfardan <hawraalfardan20@gmail.com>
2025-05-11 15:43:13 +0300

    Revert "Update cognito and Register"
    This reverts commit c18557f18f8a1e7ab55e3d2e87ffbab885502064.

--- frontend/taqyeempro/src/main.jsx
+++ frontend/taqyeempro/src/main.jsx
@@ -4,10 +4,6 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "@/components/ui/provider";
 import { BrowserRouter } from "react-router-dom";
-import { Amplify } from 'aws-amplify';
-import amplifyConfig from './auth/amplifyConfig';
-Amplify.configure(amplifyConfig);
-
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
